985190d8328feb24f4c867b77a4a203332f932ae
oussamaghandour1 <72922714+oussamaghandour1@users.noreply.github.com>
2022-04-03 23:53:06 +0200

    First Commit

---
+++ coord-year.js
@@ -0,0 +1,54 @@
+var climaticEvents = {"type":"FeatureCollection","features":[
+
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Magdeburg","occ":10}, "geometry": {"coordinates": [11.6276, 52.1205], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Arnhem","occ":10}, "geometry": {"coordinates": [5.8987, 51.9851], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Lucerne","occ":10}, "geometry": {"coordinates": [8.3093, 47.0502], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Tiel","occ":10}, "geometry": {"coordinates": [5.4279, 51.8876], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Dortmund","occ":10}, "geometry": {"coordinates": [7.4653, 51.5136], "type": "Point"}}]},
+
+
+]};
+
+var recoltesData = {"type":"FeatureCollection","features":[
+
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Metz","occ":8}, "geometry": {"coordinates": [6.1757, 49.1193], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Klosterneuburg","occ":10}, "geometry": {"coordinates": [16.5, 48.3099], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Paris","occ":10}, "geometry": {"coordinates": [2.3522, 48.8566], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Perugia","occ":10}, "geometry": {"coordinates": [12.3908, 43.1107], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"L'Aquila","occ":10}, "geometry": {"coordinates": [13.6, 42.3498], "type": "Point"}}]}
+
+
+]};
+
+var herbagesData = {"type":"FeatureCollection","features":[
+
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Mâcon","occ":8}, "geometry": {"coordinates": [4.8287, 46.3069], "type": "Point"}}]}
+
+
+]};
+
+var inondationsEau = {"type":"FeatureCollection","features":[
+
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Tours","occ":8}, "geometry": {"coordinates": [0.6848, 47.3941], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Strasbourg","occ":8}, "geometry": {"coordinates": [7.7521, 48.5734], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Bale","occ":8}, "geometry": {"coordinates": [13.7860, 45.0426], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Arles","occ":8}, "geometry": {"coordinates": [4.6278, 43.6766], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Tarascon","occ":8}, "geometry": {"coordinates": [4.6575, 43.8060], "type": "Point"}}]},
+
+]};
+
+var phenologiques = {"type":"FeatureCollection","features":[
+
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Dijon","occ":8}, "geometry": {"coordinates": [5.0415, 47.3220], "type": "Point"}}]},
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Beaune","occ":8}, "geometry": {"coordinates": [4.8400, 47.0260], "type": "Point"}}]},
+
+
+]};
+
+
+var recoltesCerealesData = {"type":"FeatureCollection","features":[
+
+{"type":"FeatureCollection","features":[{"type": "Feature","properties":{"name":"Cremona","occ":8}, "geometry": {"coordinates": [10.0227, 45.1332], "type": "Point"}}]},
+
+
+]};
